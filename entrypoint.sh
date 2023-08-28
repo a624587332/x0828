33aa3962b0eefe6cb90015e3657b85e1f89f622b
a624587332 <36526246+a624587332@users.noreply.github.com>
2023-08-28 15:09:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ TROJAN_WSPATH='/trojan'
 SS_WSPATH='/shadowsocks'
 NEZHA_SERVER='nz.kjkkk.link'
 NEZHA_PORT='5555'
-NEZHA_KEY='CaYvNQp2dlJZ7rqZfC'
+NEZHA_KEY='XawSsLx6CqcO2cT9Xz'
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
 sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
 sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
